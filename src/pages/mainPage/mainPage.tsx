f5b495112a96e71705467fa59c467ab373e09272
Хохлушин Игорь <just4-spam@mail.ru>
2023-06-27 18:58:20 +0600

    check http requests

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import './mainPage.scss';
 import {Types} from '../../utils/types'
 import {connect} from 'react-redux'
 import DescriptionComp from "../../comps/descriptionComp/descriptionComp";
 import {JokeComp} from "../../comps/jokeComp/jokeComp";
+import ActionButton from "../../comps/actionButton/actionButton";
+import apiService from "../../utils/apiService";
 
 
 
 const MainPage: React.FC<any> = (props) => {
+
+    const [users, setUsers] = useState();
+
+    const getContent = () => {
+        // @ts-ignore
+        if (users) {
+            console.log(users);
+            // @ts-ignore
+            return users[0].name;
+        }
+        return 'no users';
+    };
+    const getUsers = () => {
+        apiService.getUsers().then((users: any) => {
+            setUsers(users);
+        })
+    };
+    const auth = () => {
+        apiService.authorization({
+            login: 'joe',
+            password: '1234'
+        }).then((res: any) => {
+            console.log(res);
+        })
+    };
+
     return <div className="mainPage">
+        <div className="">{getContent()}</div>
+        <ActionButton onClick={getUsers} label={'getUsers'}/>
+        <ActionButton onClick={auth} label={'auth'}/>
             <DescriptionComp/>
             <JokeComp/>
     </div>
--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const endPointStub = '../assets/stub/';
 const randomFaceUrl = 'thisPersondoesnotexist.com/image';
 const chuckNorrisJokeUrl = 'https://api.chucknorris.io/jokes/random';
+const dishLoggerApiUrl = 'https://dish-logger.onrender.com/api/users/get_all';
+const dishLoggerApiUrlAuth = 'https://dish-logger.onrender.com/api/users/login';
+const dishLoggerProducts = 'https://dish-logger.onrender.com/api/products/get_all';
 
 const apiGetRequest = (url: string) => {
     return axios.get(url);
@@ -22,6 +25,21 @@ const getRandomJoke = () => {
     return axios.get(chuckNorrisJokeUrl).then((data: any) => Promise.resolve(data.data.value));
 };
 
+const getUsers = () => {
+    return axios.get(dishLoggerApiUrl).then((data: any) => Promise.resolve(data.data));
+};
+
+const authorization = (data: any) => {
+    // const body = JSON.stringify(data);
+    const body = data;
+    return axios.post(dishLoggerApiUrlAuth, body).then((data: any) => Promise.resolve(data.data));
+};
+const getProducts = () => {
+    // const body = JSON.stringify(data);
+    const body = data;
+    return axios.get(dishLoggerProducts).then((data: any) => Promise.resolve(data.data));
+};
+
 export default {
-    getRandomJoke
+    getRandomJoke, getUsers, authorization
 };
