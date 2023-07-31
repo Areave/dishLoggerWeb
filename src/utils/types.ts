3222ed0d11e1a650cc2351ec4afec7a43facfd92
Хохлушин Игорь <just4-spam@mail.ru>
2023-07-31 15:21:58 +0600

    Update types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,169 @@
-import React, {ReactElement} from "react";
+import React from "react";
 
 export namespace Types {
 
-    export interface State {
-        readonly user: any;
-        readonly items: any;
-        readonly messages: any;
+    // state
+
+    export interface MainState {
+        readonly user: UserState;
+        readonly items: ItemsState;
+        readonly messages: MessagesState;
+    }
+
+    export interface UserState {
+        readonly isAuthorized: boolean,
+        readonly currentUser: User,
+        readonly userStat: UserStat,
+        readonly isUserLoading: boolean,
+        readonly isUserStatLoading: boolean
+    }
+
+    export interface ItemsState {
+        readonly product: Product,
+        readonly products: Product[],
+        readonly dish: Dish,
+        readonly dishes: Dish[],
+        readonly meal: Meal,
+        readonly meals: Meal[],
+        readonly isItemLoading: boolean,
+        readonly isItemsLoading: boolean,
+    }
+
+    export interface MessagesState {
+        messages: Message[]
+    }
+
+    // entities
+    export interface User {
+        name: string,
+        login: string,
+        intakeData: {
+            energyValue: {
+                calories: number,
+                proteines: number,
+                fats: number,
+                carbohydrates: number
+            },
+        },
+        products: Product[],
+        dishes: Dish[],
+        meals: Meal[],
+    }
+
+    export interface UserStat {
+        readonly mainStat: {
+            energyValue: {
+                calories: number,
+                proteines: number,
+                fats: number,
+                carbohydrates: number
+            },
+            weight: number,
+            price: number,
+        },
+        readonly statArray: Stat[]
+    }
+
+    export interface Stat {
+        dateString: string,
+        energyValue: {
+            calories: number,
+            proteines: number,
+            fats: number,
+            carbohydrates: number
+        }
+        energyValueDifference: {
+            calories: number,
+            proteines: number,
+            fats: number,
+            carbohydrates: number
+        }
+        meals: Meal[],
+        price: number,
+        weight: number
+    }
+
+    export interface Message {
+        type: string,
+        text: string
+    }
+
+    export interface Product {
+        name: string,
+        type: string,
+        description: string,
+        amountOfItems: number,
+        weightTotal: number,
+        price: {
+            priceForOnePiece: number,
+            priceTotal: number,
+            priceFor100g: number
+        },
+        energyValueFor100g: {
+            calories: number,
+            proteines: number,
+            fats: number,
+            carbohydrates: number
+        },
+        energyValueForOneItem: {
+            calories: number,
+            proteines: number,
+            fats: number,
+            carbohydrates: number
+        },
+    }
+
+    export interface Dish {
+        name: string,
+        type: string,
+        description: string,
+        ingridients: {
+            ingridient: Product,
+            weight: number,
+            amountOfItems: number,
+            price: number,
+            energyValue: {
+                calories: number,
+                proteines: number,
+                fats: number,
+                carbohydrates: number
+            }
+        }[],
+        weight: number,
+        price: number,
+        energyValue: {
+            calories: number,
+            proteines: number,
+            fats: number,
+            carbohydrates: number
+        }
+    }
+
+    export interface Meal {
+        name: string,
+        type: string,
+        description: string,
+        dateString: string,
+        ingridients: [{
+            ingridient: Product | Dish,
+            weight: number,
+            amountOfItems: number,
+            price: number,
+            energyValue: {
+                calories: number,
+                proteines: number,
+                fats: number,
+                carbohydrates: number
+            }
+        }],
+        weight: number,
+        price: number,
+        energyValue: {
+            calories: number,
+            proteines: number,
+            fats: number,
+            carbohydrates: number
+        }
     }
 
     export interface Action {
@@ -15,32 +173,8 @@ export namespace Types {
 
     export type HOC = (Comp: React.JSXElementConstructor<any>, props: any) => React.JSXElementConstructor<any>
 
-    export interface ErrorPageProps {
-        isError: boolean,
-        children?: React.ReactNode,
-        errorMessage?: string,
-        setError: (isError: boolean, errorMessage?: string) => void
-
-    }
-    export interface PageTitleProps {
-        readonly title: string
-    }
-    export interface ComponentTitleCompProps {
-        title: string
-    }
-    export interface NavigationButtonProps {
-        route: string,
-        title: string
-    }
     export interface ActionButtonProps {
         onClick: (arg?: any) => void,
         label: string
-    }
-    export interface JokeCompProps {
-        // loadJokeUsingRedux: (joke: string) => void,
-        loadJokeUsingHook?: (joke: string) => void,
-        joke: string
-    }
-    export interface ComponentProps {
     }
 }
