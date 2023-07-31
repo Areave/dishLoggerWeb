5d8246a4bb7730626d5a3da35b37603a60f0c802
Хохлушин Игорь <just4-spam@mail.ru>
2023-07-31 20:28:36 +0600

    add meal component

--- src/comps/Meal/meal.tsx
+++ src/comps/Meal/meal.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './meal.scss'
+import {useDispatch} from "react-redux";
+import {removeNewItem} from "../../utils/store/asyncThunks";
+import {Types} from "../../utils/types";
+import apiService from "../../utils/apiService";
+import {createSetMealsAction} from "../../utils/store/actionCreators";
 
-export const Meal: React.FC<any> = ({mealData}) => {
+export const Meal: React.FC<any> = ({meal}) => {
 
-    return <div className='meal'>
-        <div>{mealData.name}</div>
+    const dispatch = useDispatch();
+    const removeFunction = apiService.removeMeal;
+    const setItemsAction = createSetMealsAction;
+    const id = meal._id;
+
+    const removeMeal = () => {
+        dispatch(removeNewItem(removeFunction, setItemsAction, id));
+    };
+
+
+    return <div className='meal d-flex'>
+        <div className="" onClick={removeMeal}>remove</div>
+        <div>{meal.name}</div>
     </div>
 };
--- src/comps/Meals/meals.tsx
+++ src/comps/Meals/meals.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './meals.scss'
 import {Meal} from "../Meal/meal";
+import {useDispatch} from "react-redux";
+import {removeNewItem} from "../../utils/store/asyncThunks";
 
 export const Meals: React.FC<any> = ({meals}) => {
 
+
     return <div className='meals'>
-        {meals ? meals.map((meal: any, index: number) => <Meal key={index} mealData={meal}/>) : <div>no meals yet</div>}
+        {meals ? meals.map((meal: any, index: number) => <Meal key={index} meal={meal}/>) : <div>no meals yet</div>}
     </div>
 };
