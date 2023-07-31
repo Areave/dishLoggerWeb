7eced21f48c5c15b23d74dc7b5b831430ac08021
Хохлушин Игорь <just4-spam@mail.ru>
2023-07-31 14:28:34 +0600

    Update loginForm.tsx

--- src/comps/LoginForm/loginForm.tsx
+++ src/comps/LoginForm/loginForm.tsx
@@ -2,51 +2,28 @@ import React from "react";
 import {Types} from "../../utils/types";
 import {LoginFormTemplate} from "./loginFormTemplate";
 import apiService from "../../utils/apiService";
-import {createAddMessageAction, createSetItemsArrayAction, setIsAuthorizedAction, setUserAction} from "../../utils/store/actionCreators";
+import {createAddMessageAction, createSetMealsAction, setIsAuthorizedAction, setUserAction} from "../../utils/store/actionCreators";
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router";
 import {useState} from "react";
 import {itemTypes} from "../../utils/itemTypes";
+import {fetchLogin} from "../../utils/store/asyncThunks";
 
 const LoginFormHOC = (Comp: React.FC<any>, props?: any) => {
 
-
-
     const dispatch = useDispatch();
-    const navigate = useNavigate();
     const [formData, setFormData] = useState({login: '', password: ''});
     const [isLoading, setIsLoading] = useState(false);
-    // const [cookies, setCookie, removeCookie] = useCookies();
 
     const login = (event: any) => {
         event.preventDefault();
         console.log(formData);
         setIsLoading(true);
-        apiService.login({
+        const loginData = {
             login: 'joe',
             password: '1234'
-        }).then(response => {
-            setIsLoading(false);
-            // const [cookies, setCookie, removeCookie] = useCookies(['jwt']);
-            if(response.message) {
-                dispatch(createAddMessageAction(response.message));
-            }
-            if (response.user) {
-                dispatch(setIsAuthorizedAction(true));
-                dispatch(setUserAction(response.user));
-                dispatch(createSetItemsArrayAction(itemTypes.MEAL, response.user.meals));
-
-                // @ts-ignore
-                // setCookie("jwt", '23423432', { path: "/" });
-                // console.log(cookies.get['jwt']);
-                navigate('/');
-            } else {
-                // const [cookies, setCookie, removeCookie] = useCookies(['jwt']);
-                // @ts-ignore
-                // console.log(cookies['jwt']);
-                dispatch(setIsAuthorizedAction(false));
-            }
-        });
+        };
+        dispatch(fetchLogin(loginData));
     };
 
     const register = (event: any) => {
@@ -56,15 +33,12 @@ const LoginFormHOC = (Comp: React.FC<any>, props?: any) => {
         });
     };
 
-
-
     const onEmailChange = (event:any) => {
         setFormData({...formData, login: event.target.value});
     };
     const onPasswordChange = (event:any) => {
         setFormData({...formData, password: event.target.value});
     };
-
 
     props = {...props, login, register, onEmailChange, onPasswordChange, isLoading};
 
