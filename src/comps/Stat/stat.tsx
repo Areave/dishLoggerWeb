4012a2b35c7cc6e3328caa17bfe612ae2d933f05
Хохлушин Игорь <just4-spam@mail.ru>
2023-07-31 14:28:05 +0600

    add stat component

--- src/comps/Stat/stat.tsx
+++ src/comps/Stat/stat.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './stat.scss'
+import {useSelector} from "react-redux";
+import {Types} from "../../utils/types";
+import Loader from "../loader/loader";
 
 export const Stat: React.FC<any> = ({mainStat, statArray}) => {
 
     console.log(statArray);
 
+    const isUserStatLoading = useSelector((state: Types.State) => {
+        return state.user.isUserStatLoading;
+    });
+
     return <div className='stat'>
-        {statArray ? statArray[0].dateString : 'no stats'}
+        {isUserStatLoading && <Loader/>}
+        {!isUserStatLoading && statArray && statArray.length && statArray[0].dateString}
     </div>
 };
