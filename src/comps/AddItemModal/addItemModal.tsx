6652458e28d2bdc766e91e84d4a1b7987bbedfc5
Хохлушин Игорь <just4-spam@mail.ru>
2023-07-31 14:28:44 +0600

    add modal component

---
+++ src/comps/AddItemModal/addItemModal.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Modal} from 'react-bootstrap';
+import './addItemModal.scss'
+import ActionButton from "../actionButton/actionButton";
+import {useSelector} from "react-redux";
+import Loader from "../loader/loader";
+
+export const AddItemModal: React.FC<any> = ({showModal, setShowModal, addMeal}) => {
+
+    const isItemLoading = useSelector((state: { items: any }) => {
+        // @ts-ignore
+        return state.items.isItemLoading;
+    });
+
+    if (isItemLoading) return <Loader/>
+
+    return <div className='addItemModal'
+                style={{display: 'block', position: 'initial'}}>
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+            {/*<Modal.Dialog>*/}
+            <Modal.Header closeButton>
+                <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>Modal body text goes here.</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <ActionButton onClick={() => {
+                }} label={'label1'}/>
+                <ActionButton onClick={() => {
+                }} label={'label2'}/>
+            </Modal.Footer>
+            {/*</Modal.Dialog>*/}
+        </Modal>
+    </div>
+};
