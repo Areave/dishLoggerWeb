8451038b3a58bce16c32282c33aa59980710dec9
Хохлушин Игорь <just4-spam@mail.ru>
2023-07-31 14:30:57 +0600

    Update mainPage.tsx

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './mainPage.scss';
 import {useDispatch, useSelector} from 'react-redux'
 import ActionButton from "../../comps/actionButton/actionButton";
 import apiService from "../../utils/apiService";
 import {
+    createSetMealsAction,
     setIsAuthorizedAction,
     setUserStatAction
 } from "../../utils/store/actionCreators";
@@ -11,49 +12,66 @@ import {RootState} from "../../utils/store";
 import {useNavigate} from "react-router";
 import {Stat} from "../../comps/Stat/stat";
 import {Meals} from "../../comps/Meals/meals";
-
+import {fetchUserStatForToday} from "../../utils/store/asyncThunks";
+import {Search} from "../../comps/Search/search";
+import Modal from 'react-bootstrap/Modal';
+import {AddItemModal} from "../../comps/AddItemModal/addItemModal";
 
 
 const MainPage: React.FC<any> = () => {
+
+    const [filteredMeals, setFilteredMeals] = useState();
 
     const dispatch = useDispatch();
 
     const navigate = useNavigate();
 
+    const [showModal, setShowModal] = useState(false);
+
     useEffect(() => {
-        const date = new Date();
-        const dateString = `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`;
-        apiService.getStatsForOneDay({dateString}).then((dailyStat) => {
-            console.log('dailyStat', dailyStat);
-            dispatch(setUserStatAction(dailyStat));
-        })
+        dispatch(fetchUserStatForToday());
     }, []);
 
     // @ts-ignore
-    const user: {
-        userStat: any;
-        name: string} = useSelector((state: RootState) => {
-            console.log('userFromUseSelect', state.user);
-        return state.user;
+    const userStat: {
+        mainStat: any,
+        statArray: any
+    } = useSelector((state: RootState) => {
+        console.log('userFromUseSelect', state.user);
+        return state.user.userStat;
     });
-    const meals = useSelector((state: {items: any}) => {
+    const meals = useSelector((state: { items: any }) => {
         // @ts-ignore
-        return state.items.allMeals;
+        return state.items.meals;
     });
 
-    const logout = () => {
-        apiService.logout().then(res => {
-            dispatch(setIsAuthorizedAction(false));
-            // removeCookie('jwt');
-            navigate('/auth');
-        });
+    const onSearchChange = (event: any) => {
+        const searchString = event.target.value;
+        if (!searchString) {
+            setFilteredMeals(meals);
+            return;
+        }
+        const filterFunc = (meal: any) => {
+            return meal.name.includes(searchString)
+                || meal.description.includes(searchString);
+        };
+        const filteredMeals = meals.filter(filterFunc);
+        setFilteredMeals(filteredMeals);
     };
 
-    // @ts-ignore
+    const openModalToAddMeal = () => {
+        setShowModal(true);
+    };
+
     return <div className="page main_page">
-        <Stat mainStat={user.userStat.mainStat} statArray={user.userStat.statArray}/>
-        <Meals meals={meals}/>
-        <ActionButton onClick={logout} label={'logout'}/>
+        <div className="main_page__content">
+            <AddItemModal showModal={showModal} setShowModal={setShowModal}/>
+            <Stat mainStat={userStat.mainStat} statArray={userStat.statArray}/>
+            <Search onSearchChange={onSearchChange}/>
+            <ActionButton onClick={openModalToAddMeal} label={'add meal'}/>
+            <Meals meals={filteredMeals || meals}/>
+        </div>
+
     </div>
 };
 
