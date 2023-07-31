75413263b146d65e1b466bca7ddd66aa31b4a1ff
Хохлушин Игорь <just4-spam@mail.ru>
2023-07-31 14:24:27 +0600

    upgrade footer

--- src/comps/footer/footerTemplate.tsx
+++ src/comps/footer/footerTemplate.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './footerTemplate.scss'
 import {Types} from '../../utils/types'
 import NavigationButton from "../navigationButton/navigationButton";
+import {Nav} from "react-bootstrap";
 
 export const FooterTemplate: React.FC<Types.ComponentProps> = (props) => {
     return <div className='footer'>
         <div className="copyright">2023</div>
+        <Nav.Link className='navigation_link ps-3' href="/about">about</Nav.Link>
     </div>
 };
