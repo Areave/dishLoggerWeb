9a0958dfe1c5eec9cb43ab795029f5de07f6c174
Хохлушин Игорь <just4-spam@mail.ru>
2023-08-03 21:10:56 +0600

    fix search bug

--- src/pages/ItemsPage/mealsPage.tsx
+++ src/pages/ItemsPage/mealsPage.tsx
@@ -3,7 +3,12 @@ import ItemsPageTemplate from "./itemsPageTemplate";
 import {useDispatch, useSelector} from "react-redux";
 import {Types} from "../../utils/types";
 import {addNewItem, fetchUserStatForToday, removeNewItem} from "../../utils/store/asyncThunks";
-import {getCreateSetItemsActionByType} from "../../utils/store/actionCreators";
+import {
+    createSetMealAction,
+    createSetMealsAction,
+    getCreateSetItemActionByType,
+    getCreateSetItemsActionByType
+} from "../../utils/store/actionCreators";
 import apiService from "../../utils/apiService";
 import {getPluralItemType, itemTypes} from "../../utils/itemTypes";
 import mockItems from '../../assets/stub/mockItemsForAdding.json'
@@ -14,10 +19,10 @@ const ItemsPageHOC = (Comp: React.FC<any>, props?: any) => {
 
     const [filteredItems, setFilteredItems] = useState();
     const [showModal, setShowModal] = useState(false);
-    const [searchString, setSearchString] = useState('');
+    const [searchString, setSearchString] = useState();
     const [newItemData, setNewItemData] = useState({});
-    const dispatch = useDispatch();
 
+    const dispatch = useDispatch();
     type ObjectKey = keyof typeof mockItems;
     const key = itemType.toLowerCase() as ObjectKey;
 
@@ -28,7 +33,8 @@ const ItemsPageHOC = (Comp: React.FC<any>, props?: any) => {
         return state.items[getPluralItemType(itemType)];
     });
 
-    const createAction = getCreateSetItemsActionByType(itemType);
+    const createSetItemsAction = getCreateSetItemsActionByType(itemType);
+    const createSetItemAction = getCreateSetItemActionByType(itemType);
     const apiMethodsObject = apiService.getApiMethodsObject(itemType);
 
     useEffect(() => {
@@ -39,37 +45,48 @@ const ItemsPageHOC = (Comp: React.FC<any>, props?: any) => {
 
     const filterItems = (searchString: string): Types.CommonEntitiesType[] => {
         const filterFunc = (item: Types.CommonEntitiesType) => {
+            console.log('from filter', item, item.name.includes(searchString))
             return item.name.includes(searchString)
                 || item.description?.includes(searchString);
         };
+        console.log(itemsArray.filter(filterFunc), searchString)
         return itemsArray.filter(filterFunc);
     };
 
     useEffect(() => {
+        if (typeof searchString === 'undefined') return;
             // @ts-ignore
             setFilteredItems(filterItems(searchString));
 
     }, [searchString]);
 
-    const openModalToAddItem = () => {
+    const openModalToAddItem = (item?: Types.CommonEntitiesType) => {
+        if (item) {
+            // @ts-ignore
+            dispatch(createSetItemAction(item));
+        }
         setShowModal(true);
     };
 
     const addItem = () => {
-        const newMeal = mockItems.meal;
-        newMeal.name = Math.random() + '' + Math.random() + Math.random() + Math.random() + Math.random() + Math.random();
+        const newItem = mockItems[key];
+        console.log(newItem)
+        newItem.name = Math.random() + ' ' + Math.random() + Math.random() + Math.random() + Math.random() + Math.random();
         // @ts-ignore
-        dispatch(createAction([...itemsArray, newMeal]));
-        dispatch(addNewItem(apiMethodsObject.addItem, createAction, {[key]: mockItems[key]}))
+        dispatch(createSetItemsAction([...itemsArray, newItem]));
+        dispatch(addNewItem(apiMethodsObject.addItem, createSetItemsAction, {[key]: mockItems[key]}))
     };
 
     const removeItem = (id: string) => {
         // @ts-ignore
-        dispatch(createAction(itemsArray.filter((item: Types.CommonEntitiesType) => item._id !== id)));
-        dispatch(removeNewItem(apiMethodsObject.removeItem, createAction, id));
+        dispatch(createSetItemsAction(itemsArray.filter((item: Types.CommonEntitiesType) => item._id !== id)));
+        dispatch(removeNewItem(apiMethodsObject.removeItem, createSetItemsAction, id));
     };
 
     const items = filteredItems ? filteredItems : itemsArray;
+
+    console.log('filteredItems', !!filteredItems);
+    console.log('items', items);
 
     const wrappedProps = {
         ...props,
