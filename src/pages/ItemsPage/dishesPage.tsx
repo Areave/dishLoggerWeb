892467c13db591704aef7867fe251b1d09e36144
Хохлушин Игорь <just4-spam@mail.ru>
2023-08-04 07:19:03 +0600

    create proxy hoc

--- src/pages/ItemsPage/dishesPage.tsx
+++ src/pages/ItemsPage/dishesPage.tsx
@@ -1,89 +1,9 @@
-import React, {useEffect, useState} from "react";
-import ItemsPageTemplate from "./itemsPageTemplate";
-import {useDispatch, useSelector} from "react-redux";
-import {Types} from "../../utils/types";
-import {addNewItem, fetchUserStatForToday, removeNewItem} from "../../utils/store/asyncThunks";
-import {getCreateSetItemsActionByType} from "../../utils/store/actionCreators";
-import apiService from "../../utils/apiService";
-import {getPluralItemType, itemTypes} from "../../utils/itemTypes";
-import mockItems from "../../assets/stub/mockItemsForAdding.json";
+import React from "react";
+import {itemTypes} from "../../utils/itemTypes";
+import {ProxyHOC} from "./proxyHOC";
 
-const ItemsPageHOC = (Comp: React.FC<any>, props?: any) => {
-
-    const itemType = itemTypes.DISH;
-
-    const [filteredItems, setFilteredItems] = useState();
-    const [showModal, setShowModal] = useState(false);
-    const [searchString, setSearchString] = useState('');
-    const [newItemData, setNewItemData] = useState({});
-    const dispatch = useDispatch();
-
-    type ObjectKey = keyof typeof mockItems;
-    const key = itemType.toLowerCase() as ObjectKey;
-
-    const userStat: Types.UserStat = useSelector((state: Types.MainState) => {
-        return state.user.userStat;
-    });
-    const itemsArray: Types.CommonEntitiesType[] = useSelector((state: {items: any}) => {
-        return state.items[getPluralItemType(itemType)];
-    });
-
-    const createAction = getCreateSetItemsActionByType(itemType);
-    const apiMethodsObject = apiService.getApiMethodsObject(itemType);
-
-    const filterItems = (searchString: string): Types.CommonEntitiesType[] => {
-        const filterFunc = (item: Types.CommonEntitiesType) => {
-            return item.name.includes(searchString)
-                || item.description.includes(searchString);
-        };
-        return itemsArray.filter(filterFunc);
-    };
-
-    useEffect(() => {
-
-            // @ts-ignore
-            setFilteredItems(filterItems(searchString));
-
-    }, [searchString]);
-
-    const openModalToAddItem = () => {
-        setShowModal(true);
-    };
-
-    const addItem = () => {
-        const newDish = mockItems.dish;
-        newDish.name = newDish.name + Math.random();
-        // @ts-ignore
-        dispatch(createAction([...itemsArray, newDish]));
-        dispatch(addNewItem(apiMethodsObject.addItem, createAction, {[key]: mockItems[key]}))
-    };
-
-    const removeItem = (id: string) => {
-        // @ts-ignore
-        dispatch(createAction(itemsArray.filter((item: Types.CommonEntitiesType) => item._id !== id)));
-        dispatch(removeNewItem(apiMethodsObject.removeItem, createAction, id));
-    };
-
-    const items = filteredItems ? filteredItems : itemsArray;
-
-    console.log('filteredItems?', !!filteredItems)
-    console.log('items', items);
-
-    const wrappedProps = {
-        ...props,
-        itemType,
-        setNewItemData,
-        addItem,
-        showModal,
-        setShowModal,
-        items,
-        userStat,
-        setSearchString,
-        openModalToAddItem,
-        removeItem
-    };
-
-    return <Comp {...wrappedProps}/>
+const ItemsPageHOC = (Comp: React.FC<any>) => {
+    return <Comp itemType={itemTypes.DISH}/>
 };
 
-export const DishesPage = () => ItemsPageHOC(ItemsPageTemplate);
+export const DishesPage = () => ItemsPageHOC(ProxyHOC);
--- src/pages/ItemsPage/productsPage.tsx
+++ src/pages/ItemsPage/productsPage.tsx
@@ -1,85 +1,9 @@
-import React, {useEffect, useState} from "react";
-import ItemsPageTemplate from "./itemsPageTemplate";
-import {useDispatch, useSelector} from "react-redux";
-import {Types} from "../../utils/types";
-import {addNewItem, removeNewItem} from "../../utils/store/asyncThunks";
-import {getCreateSetItemsActionByType} from "../../utils/store/actionCreators";
-import apiService from "../../utils/apiService";
-import {getPluralItemType, itemTypes} from "../../utils/itemTypes";
-import mockItems from '../../assets/stub/mockItemsForAdding.json'
+import React from "react";
+import {itemTypes} from "../../utils/itemTypes";
+import {ProxyHOC} from "./proxyHOC";
 
-const ItemsPageHOC = (Comp: React.FC<any>, props?: any) => {
-
-    const itemType = itemTypes.PRODUCT;
-
-    const [filteredItems, setFilteredItems] = useState();
-    const [showModal, setShowModal] = useState(false);
-    const [searchString, setSearchString] = useState('');
-    const [newItemData, setNewItemData] = useState({});
-    const dispatch = useDispatch();
-
-    type ObjectKey = keyof typeof mockItems;
-    const key = itemType.toLowerCase() as ObjectKey;
-
-    const userStat: Types.UserStat = useSelector((state: Types.MainState) => {
-        return state.user.userStat;
-    });
-    const itemsArray: Types.CommonEntitiesType[] = useSelector((state: {items: any}) => {
-        return state.items[getPluralItemType(itemType)];
-    });
-
-    const createAction = getCreateSetItemsActionByType(itemType);
-    const apiMethodsObject = apiService.getApiMethodsObject(itemType);
-
-    const filterItems = (searchString: string): Types.CommonEntitiesType[] => {
-        const filterFunc = (item: Types.CommonEntitiesType) => {
-            return item.name.includes(searchString)
-                || item.description.includes(searchString);
-        };
-        return itemsArray.filter(filterFunc);
-    };
-
-    useEffect(() => {
-        // if (searchString) {
-            // @ts-ignore
-            setFilteredItems(filterItems(searchString));
-        // }
-    }, [searchString]);
-
-    const openModalToAddItem = () => {
-        setShowModal(true);
-    };
-
-    const addItem = () => {
-        const newProduct = mockItems.productPieceAndAmount;
-        // @ts-ignore
-        dispatch(createAction([...itemsArray, newProduct]));
-        dispatch(addNewItem(apiMethodsObject.addItem, createAction, {product: newProduct}))
-    };
-
-    const removeItem = (id: string) => {
-        // @ts-ignore
-        dispatch(createAction(itemsArray.filter((item: Types.CommonEntitiesType) => item._id !== id)));
-        dispatch(removeNewItem(apiMethodsObject.removeItem, createAction, id));
-    };
-
-    const items = filteredItems ? filteredItems : itemsArray;
-
-    const wrappedProps = {
-        ...props,
-        itemType,
-        setNewItemData,
-        addItem,
-        showModal,
-        setShowModal,
-        items,
-        userStat,
-        setSearchString,
-        openModalToAddItem,
-        removeItem
-    };
-
-    return <Comp {...wrappedProps}/>
+const ItemsPageHOC = (Comp: React.FC<any>) => {
+    return <Comp itemType={itemTypes.PRODUCT}/>
 };
 
-export const ProductsPage = () => ItemsPageHOC(ItemsPageTemplate);
+export const ProductsPage = () => ItemsPageHOC(ProxyHOC);
