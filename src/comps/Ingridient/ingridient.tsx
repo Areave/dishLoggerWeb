86adb1a107baa6e5ef8f2b4e62478ed80aa2d85c
Хохлушин Игорь <just4-spam@mail.ru>
2023-08-05 16:47:17 +0600

    upgrade ingridients logic

--- src/comps/Ingridient/ingridient.tsx
+++ src/comps/Ingridient/ingridient.tsx
@@ -5,8 +5,8 @@ import {ItemType} from "../ItemType/itemType";
 export const Ingridient: React.FC<any> = ({ingridient}) => {
     return <div className='ingridient d-flex flex-wrap align-items-center mb-4'>
         <div className="ingridient-header d-flex flex-grow-1 justify-content-between  align-items-center mb-3 mb-sm-0 me-sm-2">
-            <ItemType itemType={ingridient.ingridient.type}/>
-            <div className="ingridient-name ms-2">{ingridient.ingridient.name}</div>
+            <ItemType itemType={ingridient.ingridient && ingridient.ingridient.type}/>
+            <div title={ingridient.ingridient?.name || 'deleted'} className={"ingridient-name ms-2 " + (ingridient.ingridient ? '' : 'deleted')}>{ingridient.ingridient ? ingridient.ingridient.name : 'Deleted'}</div>
             {ingridient.amountOfItems && <div className="ingridient-info__item ingridient-amount">{ingridient.amountOfItems + 'p'}</div>}
             {ingridient.weight && <div className="ingridient-info__item ingridient-weight">{ingridient.weight + 'gr'}</div>}
             <div className="ingridient-info__item ingridient-calories">{ingridient.energyValue.calories + 'cal'}</div>
--- src/pages/ItemsPage/itemsPageTemplate.tsx
+++ src/pages/ItemsPage/itemsPageTemplate.tsx
@@ -6,10 +6,12 @@ import {Search} from "../../comps/Search/search";
 import {AddItemModal} from "../../comps/AddItemModal/addItemModal";
 import {getPluralItemType, itemTypes} from "../../utils/itemTypes";
 import {ItemCard} from "../../comps/ItemCard/itemCard";
+import LoadingPage from "../loadingPage/loadingPage";
 
 
 const ItemsPageTemplate: React.FC<any> = ({
                                               itemType,
+                                              newItemData,
                                               setNewItemData,
                                               addItem,
                                               showModal,
@@ -18,19 +20,21 @@ const ItemsPageTemplate: React.FC<any> = ({
                                               userStat,
                                               setSearchString,
                                               openModalToAddItem,
-                                              removeItem
+                                              removeItem,
+                                              isItemsLoading
                                           }) => {
     // console.log('render page');
+    if (isItemsLoading) return <LoadingPage/>;
     return <div className="page items-page">
         <div className="items-page__content">
-            <AddItemModal itemType={itemType} setNewItemData={setNewItemData} addItem={addItem} showModal={showModal}
+            <AddItemModal itemType={itemType} setNewItemData={setNewItemData} newItemData={newItemData} addItem={addItem} showModal={showModal}
                           closeModal={() => setShowModal(false)}/>
             {itemType === itemTypes.MEAL && <Stat statArray={userStat.statArray}/>}
             <Search setSearchString={setSearchString}/>
             <ActionButton className='add-item__button my-3' onClick={openModalToAddItem} label={'add ' + itemType.toLowerCase()}/>
             <div className='mb-5'>
                 {items?.length ? items.map((item: any, index: number) =>
-                        <ItemCard key={index} itemType={itemType} item={item} removeItem={removeItem}/>)
+                        <ItemCard key={index} itemType={itemType} item={item} removeItem={removeItem} openModalForEdit={(e: any) => openModalToAddItem(e, item)}/>)
                     : <div className="text-center fw-bold py-3 mt-3">{'no '+ getPluralItemType(itemType) + ' yet'}</div>}
             </div>
         </div>
