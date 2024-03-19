bd6629b51d1b338383358df4cc94432aa247a34b
Хохлушин Игорь <just4-spam@mail.ru>
2024-03-19 21:11:07 +0500

    minor fix

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -34,7 +34,7 @@ const App: React.FC<any> = () => {
     }, []);
 
     useEffect(() => {
-        if (user.currentUser.login) {
+        if (user.currentUser?.login) {
             dispatch(setIsAuthorizedAction(true))}
         }, [user]);
 
